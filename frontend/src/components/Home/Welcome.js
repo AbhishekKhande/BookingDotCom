54436169bb4f06e7d551805d1880692361a3ca3f
AbhishekKhande <abhi23khande@gmail.com>
2023-03-07 16:32:18 +0530

    Not Used Code

--- frontend/src/components/Home/Welcome.js
+++ frontend/src/components/Home/Welcome.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ViewNoLog } from "../headers/ViewNoLog";
 import "./welcome.css";
+import "../../Images/bus-stop.png";
 import { Link } from "react-router-dom";
 const Welcome = () => {
   // const navigatorPages = useNavigate();
@@ -10,22 +11,34 @@ const Welcome = () => {
       <div className="services-div">
         <div className="services">
           <Link to="/bus" className="service ">
-            <div id="Bus"></div>{" "}
+            <div id="Bus">
+              <img src="frontend/src/Images/bus-stop.png" alt="" />
+            </div>{" "}
           </Link>
           <Link to="/train" className="service ">
-            <div id="Train"></div>
+            <div id="Train">
+              <img src="frontend/src/Images/bus-stop.png" alt="" />
+            </div>
           </Link>
           <Link to="/movies" className="service ">
-            <div id="Movies"></div>
+            <div id="Movies">
+              <img src="frontend/src/Images/bus-stop.png" alt="" />
+            </div>
           </Link>
           <Link to="/concerts" className="service ">
-            <div id="Concerts"></div>
+            <div id="Concerts">
+              <img src="frontend/src/Images/bus-stop.png" alt="" />
+            </div>
           </Link>
           <Link to="/flights" className="service ">
-            <div id="Flights"></div>
+            <div id="Flights">
+              <img src="frontend/src/Images/bus-stop.png" alt="" />
+            </div>
           </Link>
           <Link to="hotels" className="service ">
-            <div id="Hotels"></div>
+            <div id="Hotels">
+              <img src="frontend/src/Images/bus-stop.png" alt="" />
+            </div>
           </Link>
         </div>
       </div>
